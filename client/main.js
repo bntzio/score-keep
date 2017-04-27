c1945a092dbcc9e1c1b07e9ca4b1adae0fad25c1
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-04-27 17:52:03 -0500

    render static player data with react

--- client/main.js
+++ client/main.js
@@ -1,22 +1,38 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+const players = [
+  {
+    _id: '1',
+    name: 'Enrique',
+    score: 96
   },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+  {
+    _id: '2',
+    name: 'Sebastian',
+    score: 88
   },
+  {
+    _id: '3',
+    name: 'Kasper',
+    score: 80
+  }
+];
+
+const renderPlayers = function(playersList) {
+  return playersList.map(function(player) {
+    const { name, score } = player;
+    return <p key={player._id}>{name} has {score} point(s).</p>;
+  });
+};
+
+Meteor.startup(function() {
+  let jsx = (
+    <div>
+      <p>{ name }</p>
+      { renderPlayers(players) }
+    </div>
+  );
+  ReactDOM.render(jsx, document.getElementById('app'));
 });
